6c59edd7fad5cb993dfbde275daae2d1646e04e0
Parker Cox <Parkercox98@gmail.com>
2019-02-21 12:32:34 -0600

    I made a test

--- Chris_Parker_Assignment3/Chris_Parker_Assignment3/Program.cs
+++ Chris_Parker_Assignment3/Chris_Parker_Assignment3/Program.cs
@@ -18,6 +18,7 @@ namespace Chris_Parker_Assignment3
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            //test test test
         }
     }
 }
